2bd87e01b8e34933c748f9b9a0fff444fbf4838a
Jamyth Luk <jamythluk@jamyth.local>
2021-08-10 12:04:59 +0800

    [UPDATE]: update deps and image rules

--- src/WebpackConfigGenerator/Rule/image.rule.ts
+++ src/WebpackConfigGenerator/Rule/image.rule.ts
@@ -1,9 +1,24 @@
 import type webpack from "webpack";
 import {RegExpUtil} from "./RegExpUtil";
 
+/**
+ * Handles dependency requests to image assets (".png", ".jpeg", ".jpg", ".gif", ".svg")
+ * by inlining as images as DataURL,
+ * or emitting as separate files if file size is too large.
+ *
+ * @see https://webpack.js.org/guides/asset-modules/
+ */
 export function imageRule(): webpack.RuleSetRule {
     return {
         test: RegExpUtil.fileExtension(".png", ".jpeg", ".jpg", ".gif", ".svg"),
-        type: "asset/resource",
+        type: "asset",
+        generator: {
+            filename: "static/img/[name][hash:8][ext][query]",
+        },
+        parser: {
+            dataUrlCondition: {
+                maxSize: 1024,
+            },
+        },
     };
 }
--- src/util/createConsoleLogger.ts
+++ src/util/createConsoleLogger.ts
@@ -2,12 +2,13 @@ import chalk from "chalk";
 
 export function createConsoleLogger(title: string) {
     const print = (emoji: string) => (color: "blueBright" | "greenBright" | "redBright") => {
-        return (title: string) => (text: string | Error | (string | Error)[]): void => {
-            const _title = chalk[color].bold(`${emoji} [${title}]`);
-            const body = chalk.whiteBright.bgBlack((Array.isArray(text) ? text : [text]).map((_) => _.toString()).join(" "));
-            console.info("");
-            console.info(`${_title} ${body}`);
-        };
+        return (title: string) =>
+            (text: string | Error | (string | Error)[]): void => {
+                const _title = chalk[color].bold(`${emoji} [${title}]`);
+                const body = chalk.whiteBright.bgBlack((Array.isArray(text) ? text : [text]).map((_) => _.toString()).join(" "));
+                console.info("");
+                console.info(`${_title} ${body}`);
+            };
     };
 
     return {
